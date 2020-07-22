ae23b460257e28ccfd986ec93752cc87f0aad9b5
Yahaya <yasholma1@gmail.com>
2020-07-22 11:56:38 +0100

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$(function () {
+  // Smooth Scrolling
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault()
+
+      const hash = this.hash
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000,
+      )
+    }
+  })
+})
+
+/* =================================================== 
+                    Portfolio
+   ================================================== */
+
+$(window).on('load', () => {
+  // Initialize Isotope
+  $('#isotope-container').isotope({})
+
+  // filtering on button click
+  $('#isotope-filters').on('click', 'button', function () {
+    // get data-filter value
+    const filterValue = $(this).attr('data-filter')
+
+    // filter portfolio items
+    $('#isotope-container').isotope({
+      filter: filterValue,
+    })
+
+    // active button
+    $('#isotope-filters').find('.active').removeClass('active')
+    $(this).addClass('active')
+  })
+})
+
+/* =================================================== 
+                    Magnifier
+   ================================================== */
+$(function () {
+  $('#portfolio-wrapper').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  })
+})
+
+/* =================================================== 
+                    AOS
+   ================================================== */
+AOS.init()
